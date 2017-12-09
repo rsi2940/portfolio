3ded80d28a64d41eacdcfc52970193aa07361b96
rsi2940 <rishi2940@gmail.com>
2017-12-09 15:14:34 -0500

    added form and added lay out for media quries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './/app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -18,7 +19,7 @@ import { ProfileComponent } from './profile/profile.component';
     AboutComponent,
     ProfileComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  clientName: string;
+  clientMessage: string;
+  clientEmail: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onCancel() {
+    console.log(this.clientEmail);
+    this.clientEmail = '';
+    this.clientMessage = '';
+    this.clientName = '';
   }
-
 }

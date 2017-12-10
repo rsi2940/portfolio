d93dd41aeaec6b0ef0677a51948145889dea9b55
rsi2940 <rishi2940@gmail.com>
2017-12-10 10:46:49 -0500

    form validation client side

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,6 +9,10 @@ export class ContactComponent implements OnInit {
   clientName: string;
   clientMessage: string;
   clientEmail: string;
+  success: boolean;
+  cm: boolean;
+  ce: boolean;
+  cn: boolean;
   constructor() {}
 
   ngOnInit() {
@@ -18,16 +22,48 @@ export class ContactComponent implements OnInit {
     this.clientEmail = '';
     this.clientMessage = '';
     this.clientName = '';
+    this.success = false;
+    this.ce = false;
+    this.cn = false;
+    this.cm = false;
+    console.log(this.cm);
   }
   onSubmit() {
-    if (!this.clientName) {
+    this.clientEmail = this.clientEmail.trim();
+    this.clientMessage = this.clientMessage.trim();
+    this.clientName = this.clientName.trim();
+
+    if (!this.clientName || this.clientName === '** required **') {
       this.clientName = '** required **';
+      this.cn = false;
+    } else {
+      this.cn = true;
     }
-    if (!this.clientMessage) {
+    if (!this.clientMessage || this.clientMessage === '** required **') {
       this.clientMessage = '** required **';
+      this.cm = false;
+      // console.log(this.cm + 'if');
+    } else {
+      this.cm = true;
+      console.log(this.cm);
     }
-    if (!this.clientEmail) {
+    if (!this.clientEmail || this.clientEmail === '** required **') {
       this.clientEmail = '** required **';
+      this.ce = false;
+    } else {
+      this.ce = true;
+    }
+    if (!this.clientEmail.includes('@')) {
+      this.clientEmail = '** must be an Email **';
+      this.ce = false;
+    } else {
+      this.ce = true;
+    }
+    if (this.ce && this.cm && this.cn) {
+      this.success = true;
+      setTimeout(() => {
+        this.onCancel();
+      }, 1000);
     }
   }
 }
